b938602729ab2d718321b833adac278d3407a69c
thalles-victor <thalles.victor2012@gmail.com>
2022-03-18 21:01:51 -0300

    primeira versão

---
+++ data.js
@@ -0,0 +1,27 @@
+export const recipe = [
+  {
+    name: 'Petisco com Vina ',
+    urlImage: 'https://s2.glbimg.com/AXaoYCopgcL0wELPkAR1fdtjYSI=/0x0:1080x608/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2022/f/X/zBfiNAR8KZu6QgTgQnSg/capa-materia-gshow-2022-01-15t154112.695.png'
+  },
+  {
+    name: 'Stick de Queijo com Salsicha',
+    urlImage: 'https://pubimg.band.uol.com.br/files/06a517acb548ae949b0e.png'
+  },
+  {
+    name: 'Filé de Peixe no Forno',
+    urlImage: 'https://img.itdg.com.br/tdg/images/recipes/000/019/007/73152/73152_original.jpg'
+  },
+  {
+    name: 'Donuts Caseiro',
+    urlImage: 'https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2008/06/donuts_caseiro.jpg'
+  },
+  {
+    name: 'Costela no Bafo Especial',
+    urlImage: 'https://www.comidaereceitas.com.br/wp-content/uploads/2010/12/costela_bafo.jpg'
+  },
+  {
+    name: 'Bolinho de Abóbora com Carne Seca',
+    urlImage: 'https://www.receitasdecomida.com.br/wp-content/uploads/2014/05/bolinho-de-abobora-com-carne-seca-9337.jpg'
+  },
+
+];
